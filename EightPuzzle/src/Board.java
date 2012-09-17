4608c334ec81a4bc37ab68f23c81e8e5362a5cd5
Erik <Erik@Ultra-pila>
2012-09-17 15:02:27 +0200

    Commited EightPuzzle scaffold and datasets.

---
+++ EightPuzzle/src/Board.java
@@ -0,0 +1,55 @@
+/**
+ * @author Erik
+ *
+ */
+public class Board {
+	private int N = 0;
+	
+	public Board(int[][] blocks) {// construct a board from an N-by-N array of blocks (where blocks[i][j] = block in row i, column j)
+
+	}; 
+
+	public int dimension() {
+		return 0; // board dimension N
+	};
+
+	public int hamming() {
+		return 0; // number of blocks out of place
+	};
+
+	public int manhattan() {
+		return 0; // sum of Manhattan distances between blocks and goal
+	};
+
+	public boolean isGoal() {
+		return false; // is this board the goal board?
+	};
+
+	public Board twin() {
+		return null; // a board obtained by exchanging two adjacent blocks in the same row
+	};
+
+	public boolean equals(Object y) {
+		return false; // does this board equal y?
+	};
+
+	public Iterable<Board> neighbors() {
+		return null; // all neighboring boards
+	};
+
+	public String toString() { // string representation of the board (in the output format specified below)
+		StringBuilder s = new StringBuilder();
+		s.append(N + "\n");
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				s.append(String.format("%2d ", tiles[i][j]));
+			}
+			s.append("\n");
+		}
+		return s.toString();
+	};
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ EightPuzzle/src/Solver.java
@@ -0,0 +1,52 @@
+/**
+ * @author Erik
+ * 
+ */
+public class Solver {
+
+	private static final String INPUT = "";
+
+	public Solver(Board initial) { // find a solution to the initial board (using the A* algorithm)
+
+	};
+
+	public boolean isSolvable() {
+		return false; // is the initial board solvable?
+
+	};
+
+	public int moves() {
+		return 0; // min number of moves to solve initial board; -1 if no solution
+
+	};
+
+	public Iterable<Board> solution() {
+		return null; // sequence of boards in a shortest solution; null if no solution
+
+	};
+
+	public static void main(String[] args) { // solve a slider puzzle (given below)
+		// create initial board from file
+		// In in = new In(args[0]);
+		In in = new In(INPUT);
+		int N = in.readInt();
+		int[][] blocks = new int[N][N];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
+
+		// solve the puzzle
+		Solver solver = new Solver(initial);
+
+		// print solution to standard output
+		if (!solver.isSolvable())
+			StdOut.println("No solution possible");
+		else {
+			StdOut.println("Minimum number of moves = " + solver.moves());
+			for (Board board : solver.solution())
+				StdOut.println(board);
+		}
+	}
+
+}
